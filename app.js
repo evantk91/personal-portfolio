b53ec8546142ee6849e1576256e6d276e3a39300
etgreer91 <etgreer91@gmail.com>
2020-12-08 19:07:57 -0700

    updates blog posts

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const context = {
     blogs: [
         {
+            title: "Least Recently Used Cache",
+            url: "https://etgreer91.medium.com/least-recently-used-cache-b5a98f0d0cac",
+            photo_src: "./photos/gary-chan-YzSZN3qvHeo-unsplash.jpg" 
+        },
+        {
             title: "How to Render Likes on a Page",
             url: "https://medium.com/@etgreer91/how-to-render-likes-on-a-page-4d660e7ea5e4",
             photo_src: "./photos/prateek-katyal-xv7-GlvBLFw-unsplash.jpg" 
@@ -19,11 +24,6 @@ const context = {
             title: "The Problem Solving Process Using Mocha Testing",
             url: "https://medium.com/@etgreer91/the-problem-solving-process-using-mocha-testing-ac829f63b108",
             photo_src: "./photos/mocha_testing_blog.png"
-        },
-        {
-            title: "How to Unroll a Matrix: a Look into a Common Interview Question",
-            url: "https://medium.com/@etgreer91/how-to-unroll-a-matrix-a-look-into-a-common-interview-question-61684e270f03",
-            photo_src: "./photos/unroll_a_matrix.jpeg"
         }
     ]
 }
